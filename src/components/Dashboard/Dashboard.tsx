01adcae1e1ec03de30d1f7520de7198d2bb8afd7
Xopar <xoparr@gmail.com>
2023-11-01 17:09:02 -0500

    Moved listener functions from memo to effect to properly be updated and cleaned up as relevant state variables change

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -3,8 +3,8 @@
 import LoginMenu from "@components/LoginMenu";
 import PlayerPresenceBar from "@components/PlayerPresenceBar";
 import TextClient from "@components/TextClient";
-import { Client, ITEM_FLAGS, PRINT_JSON_TYPE, PrintJSONPacket } from "archipelago.js";
-import { useMemo, useRef, useState } from "react";
+import { Client, ConnectedPacket, ITEM_FLAGS, PRINT_JSON_TYPE, PrintJSONPacket, ServerPacket } from "archipelago.js";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 const Dashboard: React.FC<{}> = () => {
 
@@ -17,17 +17,38 @@ const Dashboard: React.FC<{}> = () => {
   // Set up the AP client connection
   const client = useMemo(() => {
     const apClient = new Client();
+    return apClient;
+  }, [])
 
-    apClient.addListener("PrintJSON", (packet) => {
+
+  // Handle Lisener: Connected
+  useEffect(() => {
+    const parseConnected = (packet: ConnectedPacket) => {
+      console.log(packet)
+      if (!isConnected) {
+        setTrackSlots([packet.slot]);
+        setMyConnectedSlot(packet.slot);
+        setIsConnected(true);
+        client.say("!players")
+      }
+    };
+    client.addListener("Connected", parseConnected);
+    return () => client.removeListener("Connected", parseConnected);
+  }, [client, isConnected]);
+
+
+  // Handle Listener: PrintJSON
+  useEffect(() => {
+    const parsePrintJSON = (packet: PrintJSONPacket) => {
       console.log(packet);
 
       if (packet.type === "CommandResult") {
         // This is super jank and sketch but there is no good way to get the connected playerlist sooooo...
-        const nPlayers = apClient.players.all.length - 1;
+        const nPlayers = client.players.all.length - 1;
         if (packet.data[0].text.includes(` players of ${nPlayers} connected `)) {
           const updatingActivePlayers = [];
           for (let i = 0; i < nPlayers; i++) {
-            const onePlayer = apClient.players.name(i+1);
+            const onePlayer = client.players.name(i+1);
             if (!packet.data[0].text.includes(`(${onePlayer})`)) {
               updatingActivePlayers.push(i+1)
             }
@@ -36,30 +57,44 @@ const Dashboard: React.FC<{}> = () => {
         }
       }
 
-      const msgDOMElement = formatMessage(packet, apClient);
-      msgHistoryRef.current?.appendChild(msgDOMElement);
-    });
-
-    apClient.addListener("Connected", (packet) => {
-      console.log(packet)
-      if (!isConnected) {
-        setTrackSlots([packet.slot]);
-        setMyConnectedSlot(packet.slot);
-        setIsConnected(true);
-        apClient.say("!players")
+      // Handle Join/Part for prescense bar
+      if (packet.type === "Join" || packet.type === "Part") {
+          const changingSlot = packet.slot;
+          const newActivePlayers = [ ...activePlayers ];
+          if (packet.type === "Join") {
+            if (!newActivePlayers.includes(changingSlot)) {
+              newActivePlayers.push(changingSlot);
+              setActivePlayers(newActivePlayers);
+            }
+          } else {
+            if (newActivePlayers.includes(changingSlot)) {
+              const i = newActivePlayers.indexOf(changingSlot)
+              newActivePlayers.splice(i, 1);
+              setActivePlayers(newActivePlayers);
+            }
+          }
       }
-    });
 
-    apClient.addListener("PacketReceived", (packet) => {
+      const msgDOMElement = formatMessage(packet, client);
+      msgHistoryRef.current?.appendChild(msgDOMElement);
+    }
+    client.addListener("PrintJSON", parsePrintJSON);
+
+    return () => client.removeListener("PrintJSON", parsePrintJSON);
+  }, [client, activePlayers]);
+
+
+  // Handle Listener: PacketReceived
+  useEffect(() => {
+    const parsePacketReceived = (packet: ServerPacket) => {
       if (packet.cmd === "Connected") {}
       else if (packet.cmd === "PrintJSON") {}
-      else {
-        console.log(packet)
-      }
-    });
+      else { console.log(packet) }
+    }
+    client.addListener("PacketReceived", parsePacketReceived);
+    return () => client.removeListener("PacketReceived", parsePacketReceived);
+  }, [client]);
 
-    return apClient;
-  }, [])
 
   const formatMessage = (packet: PrintJSONPacket, client: Client): HTMLDivElement => {
     const msgDOMElement = document.createElement("div");
